bfaec0257412f8aa76beec30ec5fcd0e9cca39c6
avue7 <avue7@mail.csuchico.edu>
2019-05-23 10:21:08 -0700

    Google plus log in successful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { Platform } from '@ionic/angular';
+import { Platform, LoadingController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -25,15 +29,34 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private nativeStorage: NativeStorage,
+    private googlePlus: GooglePlus,
+    private loadingCtrl: LoadingController,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      //this.statusBar.styleDefault();
+      //this.splashScreen.hide();
+      this.isLoggedIn();
     });
   }
+
+  isLoggedIn(){
+    this.nativeStorage.getItem('google_user')
+    .then (data => {
+      this.router.navigate(["/home"]);
+      this.splashScreen.hide();
+    }, error => {
+      this.router.navigate(["/login"]);
+      this.splashScreen.hide();
+    });
+    this.statusBar.styleDefault();
+  }
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAmRpKZou29zyNtRd5hS9U4TwBhT2WkXns",
+    authDomain: "ontimeme-c948f.firebaseapp.com",
+    databaseURL: "https://ontimeme-c948f.firebaseio.com",
+    projectId: "ontimeme-c948f",
+    storageBucket: "ontimeme-c948f.appspot.com",
+    messagingSenderId: "955792506678",
+    appId: "1:955792506678:web:2680c8cbefef40e0"
+  }
+}
